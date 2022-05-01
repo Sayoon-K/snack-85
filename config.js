df6756afb7b6082e319b03d0748a3e1542f46434
Sayoon-K <84568721+Sayoon-K@users.noreply.github.com>
2022-05-01 14:44:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCJeB7_TQMrmUlMo_FIh__QEOeYnnJ8BWI",
+  authDomain: "storytelling-app-def33.firebaseapp.com",
+  projectId: "storytelling-app-def33",
+  storageBucket: "storytelling-app-def33.appspot.com",
+  messagingSenderId: "986908832020",
+  appId: "1:986908832020:web:8c96f026c1de81accd1201"
+};
